3a5142fa917fd068e6ba3a58edad9957d68ab26b
Justin Evans <justin@whoat.net>
2017-09-11 16:15:13 -0400

    Initial commit. Configured Webpack

---
+++ src/client/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+import './App.scss';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Discovery Assignment</h1>
+      </div>
+    );
+  }
+}
